5d82b33d9e4e4f99ddf739ef00ee3c20242fbf32
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-14 22:50:20 -0500

    moved code to top to try to get heroku to work

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ var route             = require('./controllers/burgers_controller.js');
 var app               = express();
 
 var PORT = process.env.PORT || 8080;
-
+route.burgerController(app);
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-route.burgerController(app);
+
 
 app.listen(PORT, function() {
   console.log('Listening on %s', PORT);
